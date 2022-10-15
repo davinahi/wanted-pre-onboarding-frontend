de1d7e12ed832d5fdfb894057e01f80eb5794aaa
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-15 15:41:06 +0900

    The function of CRUD is completed but need refactorig

--- todo-list/src/components/Todos/Item.jsx
+++ todo-list/src/components/Todos/Item.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styled from "styled-components";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -7,7 +7,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const Item = ({ todo, onToggle, onRemove }) => {
   const [edited, setEdited] = useState(false);
   const [val, setVal] = useState(todo.content);
-  const [confirmEdit, setConfirmEdit] = useState(false);
+  const inputEl = useRef();
 
   const checkBoxClickHandler = () => {
     onToggle(todo.id);
@@ -23,63 +23,98 @@ const Item = ({ todo, onToggle, onRemove }) => {
 
   const inputHandler = (e) => {
     setVal(e.target.value);
+    inputEl.current.focus();
   };
 
   const stopHandler = (e) => {
     e.stopPropagation();
   };
 
-  const confirmEditHandler = () => {
-    setConfirmEdit(!confirmEdit);
+  const ConfirmClickHandler = () => {
+    setVal(val);
+    setEdited(!edited);
+    // 변경된 값을 label에 넘겨줘야함.
   };
 
+  const cancelClickHandler = () => {
+    setEdited(false);
+  };
+
+  console.log(val);
+
   return (
-    <Li>
-      <Checkbox onClick={checkBoxClickHandler}>
-        {todo.checked ? (
-          <span className="edit-btn" role="button">
-            <i className="bi bi-check-square"></i>
-          </span>
-        ) : (
-          <span className="edit-btn" role="button">
-            <i className="bi bi-square"></i>
-          </span>
-        )}
-
-        {edited ? (
-          <input
-            className="edit-input"
-            onChange={inputHandler}
-            onClick={stopHandler}
-            value={val}
-          ></input>
-        ) : (
-          <Label forhtml="todoItem">{todo.content}</Label>
-        )}
-      </Checkbox>
-
-      {/* <input type="text" id="todoItem" /> */}
-
+    <>
       {edited ? (
-        <div className="edit-mode__btns">
-          <span className="confirm-btn" role="button">
-            <i className="bi bi-check-circle"></i>
-          </span>
-          <span className="cancel-btn" role="button">
-            <i className="bi bi-x-circle"></i>
-          </span>
-        </div>
+        <Li>
+          <Checkbox onClick={checkBoxClickHandler}>
+            {todo.checked ? (
+              <span className="edit-btn" role="button">
+                <i className="bi bi-check-square"></i>
+              </span>
+            ) : (
+              <span className="edit-btn" role="button">
+                <i className="bi bi-square"></i>
+              </span>
+            )}
+
+            <input
+              className="edit-input"
+              onChange={inputHandler}
+              onClick={stopHandler}
+              value={val}
+              // ref={inputEl}
+            ></input>
+          </Checkbox>
+
+          <div className="edit-mode__btns">
+            <span
+              className="confirm-btn"
+              role="button"
+              onClick={ConfirmClickHandler}
+            >
+              <i className="bi bi-check-circle"></i>
+            </span>
+            <span
+              className="cancel-btn"
+              role="button"
+              onClick={cancelClickHandler}
+              // onClick={confirmEditHandler}
+            >
+              <i className="bi bi-x-circle"></i>
+            </span>
+          </div>
+        </Li>
       ) : (
-        <div className="edit__btns">
-          <span className="edit-btn" role="button" onClick={editContentHandler}>
-            <i className="bi bi-pencil-square"></i>
-          </span>
-          <span className="remove-btn" role="button" onClick={removeHandler}>
-            <i className="bi bi-trash3"></i>
-          </span>
-        </div>
+        <Li>
+          <Checkbox onClick={checkBoxClickHandler}>
+            {todo.checked ? (
+              <span className="edit-btn" role="button">
+                <i className="bi bi-check-square"></i>
+              </span>
+            ) : (
+              <span className="edit-btn" role="button">
+                <i className="bi bi-square"></i>
+              </span>
+            )}
+
+            <Label forhtml="todoItem">{val}</Label>
+          </Checkbox>
+
+          <div className="edit__btns">
+            <span
+              className="edit-btn"
+              role="button"
+              onClick={editContentHandler}
+            >
+              <i className="bi bi-pencil-square"></i>
+            </span>
+            <span className="remove-btn" role="button" onClick={removeHandler}>
+              <i className="bi bi-trash3"></i>
+            </span>
+          </div>
+        </Li>
       )}
-    </Li>
+    </>
   );
 };
 
